c78535985223ba81c7557bf3c0cc60cd144bbeb4
Axel <114549360+AxelDev1996@users.noreply.github.com>
2024-01-05 23:21:43 -0300

    updating the directory page with categories-container and category-item components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import './categories.styles.scss';
+import CategoriesContainer from './components/categories-container/categories-container.component';
 
-function App() {
+const App = () => {
+
+
+  
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CategoriesContainer />
   );
 }
-
 export default App;
